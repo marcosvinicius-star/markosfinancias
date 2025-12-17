3237821c194b6dbf41e4c53cbca73a0aca32cd08
markkos <marcosvinicius@evacloudd.com>
2025-12-17 10:53:49 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -24,14 +24,14 @@
 
 window.FIREBASE_CONFIG = window.FIREBASE_CONFIG || {
   // OBRIGATÓRIO (cole do Firebase Console)
-  apiKey: "",
+  apiKey: "AIzaSyDufoi9Lq0Pg9zA_kRZUpEB-01n7baHgRY",
 
   // Derivados do projectId
   authDomain: "financeflow-eda21.firebaseapp.com",
   projectId: "financeflow-eda21",
-  storageBucket: "financeflow-eda21.appspot.com",
+  storageBucket: "financeflow-eda21.firebasestorage.app",
 
   // Recomendado (cole do Firebase Console)
-  messagingSenderId: "",
-  appId: ""
+  messagingSenderId: "898938140466",
+  appId: "1:898938140466:web:ba62b7f99e3db3feda892b"
 };
